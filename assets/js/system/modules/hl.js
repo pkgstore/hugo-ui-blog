e70631477c0e371ba43b4d474ffbbd498cbb7ffd
KaiKimera <git@kai.kim>
2024-04-16 17:01:50 +0300

    Update hlJS

--- assets/js/system/modules/hl.js
+++ assets/js/system/modules/hl.js
@@ -5,14 +5,11 @@ export function init() {
 function highlight() {
   const $el = document.querySelectorAll('pre code');
   const $len = $el.length;
-  const $codeDataList = [];
-  const $codeList = [];
+  let $codeDataList = [], $codeList = [], $code = [], $lang =[];
 
   for (let $i = 0; $i < $len; ++$i) {
-    let $code = $el[$i];
-    let $lang = Array.from($code.classList).find((c) =>
-      c.startsWith('language-')
-    );
+    $code = $el[$i];
+    $lang = [...$code.classList].find(($c) => $c.startsWith('language-'));
 
     if (!$lang) $lang = 'plaintext';
 
@@ -28,14 +25,9 @@ function highlight() {
   $worker.onmessage = ($event) => {
     for (let $i = 0; $i < $len; ++$i) {
       $codeList[$i].innerHTML = $event.data[$i];
+      $codeList[$i].classList.add('hljs');
+      $codeList[$i].classList.add('language-' + $codeDataList[$i].language);
     }
-
-    // $codeList.forEach(($code, $i) => {
-    //   $code.innerHTML = $event.data[$i];
-    //   $code.setAttribute('data-highlighted', 'yes');
-    //   $code.classList.add('hljs');
-    //   $code.classList.add('language' + $codeDataList[$i].language);
-    // });
   }
 
   $worker.postMessage($codeDataList);
